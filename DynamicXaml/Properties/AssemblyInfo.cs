c08c1540be239f997fa5ca0a051f67fe75af6a43
flq <fquednau@gmail.com>
2012-02-20 15:13:43 +0100

    version update

--- DynamicXaml/Properties/AssemblyInfo.cs
+++ DynamicXaml/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.6.1")]
-[assembly: AssemblyFileVersion("0.6.1")]
+[assembly: AssemblyVersion("0.6.2")]
+[assembly: AssemblyFileVersion("0.6.2")]
